e7b0f064a157f9537c7e7705ee7b87f6a6453a45
Eduarda Timóteo <eduarda.silva7739@gmail.com>
2025-05-22 22:28:19 -0300

    Add files via upload

---
+++ desafio09.py
@@ -0,0 +1,15 @@
+tabuada = int(input('Digite um valor para ver sua tabuada:')) 
+
+tabu1 = tabuada * 1
+tabu2 = tabuada * 2
+tabu3 = tabuada * 3
+tabu4 = tabuada * 4
+tabu5 = tabuada * 5
+tabu6 = tabuada * 6
+tabu7 = tabuada * 7
+tabu8 = tabuada * 8
+tabu9 = tabuada * 9
+tabu10 = tabuada * 10
+
+
+print (f' \n{tabuada} x 1 = {tabu1} \n {tabuada} x 2 = {tabu2}\n {tabuada} x 3 = {tabu3} \n {tabuada} x 4 = {tabu4}\n {tabuada} x 5 = {tabu5}\n {tabuada} x 6 = {tabu6}\n {tabuada} x 7 = {tabu7}\n {tabuada} x 8 = {tabu8}\n {tabuada} x 9 = {tabu9} \n {tabuada} x 10 = {tabu10} ')
